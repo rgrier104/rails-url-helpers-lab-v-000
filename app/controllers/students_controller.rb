62ab1d1539ac51749fdebcc6e82cae7e3aeb66c5
Rachel Grier <rgrier104@gmail.com>
2019-09-10 21:35:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -12,5 +12,9 @@ class StudentsController < ApplicationController
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def activate
+      
     end
 end
